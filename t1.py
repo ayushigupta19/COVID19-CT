89cb4e3c658c9910acc41a5995b2a9753ec0f70d
ayushigupta19 <ayushi19isha@gmail.com>
2020-04-26 17:39:30 +0530

    added code for test run

--- t1.py
+++ t1.py
@@ -2,5 +2,5 @@ from simulation import simulate
 
 output = []
 init_cond = [[56], [84], [13], [12], [88]]
-simulate(init_cond, output, path="output1.csv", algo_mode='level3', population=100, days=80, tstamp_per_day=40)
+simulate(init_cond, output, path="output1.csv", algo_mode='level3', population=300, days=100, tstamp_per_day=40)
 print("Final output: ", output)
